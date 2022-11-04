54db1f499603c90dd835b4f268955781176a2f45
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2022-11-04 14:10:33 +0100

    formatting

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,14 @@ import './style.css';
 
 import { pipe } from 'lodash/fp';
 
-export {};
-
 const text = '    JavaScript    ';
 
 const toLower = (input: string) => input.toLowerCase();
-
 const trim = (input: string) => input.trim();
-
 const putInElement = (element: string) => (input: string) => `<${element}>${input}</${element}>`;
 
 const output = trim(toLower(text));
-
 const transform = pipe(trim, toLower, putInElement('div'));
 
 console.log(output);
-
 console.log(transform(text));
