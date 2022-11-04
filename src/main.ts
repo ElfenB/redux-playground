c6b000e0ac49da97bec35d498f04d874aa6fa6a3
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2022-11-04 16:05:05 +0100

    added spread test

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,12 @@ const transform = pipe(trim, toLower, putInElement('div'));
 
 console.log(output);
 console.log(transform(text));
+
+// --------------------------------------
+
+const personA = { name: 'Anna', address: { street: 'Alter Weg', town: 'Altstadt' } };
+const personB = { ...personA, name: 'Bob' };
+personB.address.street = 'Neuer Weg';
+
+console.log('personA', JSON.stringify(personA.address.street));
+console.log('personB', JSON.stringify(personB.address.street));
